0b69408df0426776c8ee1f4dc058b484004daaa8
OlenaNechaiStarGlobal <onechai@star.global>
2023-03-19 23:39:36 +0100

    homework 16

---
+++ homework_16/css.py
@@ -0,0 +1,19 @@
+# Find web page.
+# Write 30 XPATH locators for this page using XPath Axes and Wildcards. Some of them should have more than 3 steps.
+# For 10 XPATH locators write 10 CSS locators which find the same element
+
+"""
+web_page = 'https://www.svz.io//'
+
+div.navigation.w-nav
+.hero-copy
+.grain.faint
+.slider.w-slider
+#w-slider-mask-0
+div.navbar_container-2 > a > img
+div.slider.w-slider
+div.webflow-experts > img
+#Contact
+#Budget
+
+"""
---
+++ homework_16/xpath.py
@@ -0,0 +1,39 @@
+# Find web page.
+# Write 30 XPATH locators for this page using XPath Axes and Wildcards. Some of them should have more than 3 steps.
+# For 10 XPATH locators write 10 CSS locators which find the same element
+
+"""
+web_page = 'https://www.svz.io//'
+
+//*[@data-animation='default']
+//*[@class='hero-copy']
+//*[@aria-label='carousel']
+//*[@id='w-slider-mask-0']
+//*[@alt='SVZ']
+//*[@role="banner"]
+//*[@data-delay='4000']
+//*[@alt='Webflow Experts']
+//*[@alt='Webflow Experts']/parent::*/child::*[@class='grain faint']
+//div[@data-animation='default']/child::div[@class='navbar_container-2']
+//div[@class='hero-copy']/ancestor::*[@class='container-main']
+//div[@aria-label='carousel']/parent::div[@class='border-block testimonial']
+//div[@id='w-slider-mask-0']/descendant::*[@aria-label='1 of 3']
+//img[@alt='SVZ']/preceding::*[@class='preloader']
+//div[@role="banner"]/following-sibling::section[@id='Contact']
+//*[@data-delay='4000']/parent::*
+//*[@alt='Webflow Experts']/self::*
+//h2[text()='Suite']
+//h2[text()='Suite']/ancestor::body/parent::*
+//p/child::a
+//title
+//title/following-sibling::meta[@content='website']
+//*[text()='Webflow']/parent::*
+//*[@tabindex='0' and @role='button' and @aria-pressed='true']
+//*[@class='faqs-block']
+//*[@class='faqs-block']/child::*[@class='faq-lottie']
+//*[@id='Budget']
+//*[@id='Budget']/ancestor::section[@id='Contact']
+//p[contains(text(), 'amazing')]
+//p[contains(text(), 'amazing')]/parent::div/parent::*[@role='list']
+
+"""
